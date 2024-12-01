72ed4cbea960110deb3cb74b0f35663219577869
Enea <eneazani2005@gmail.com>
2024-12-01 10:48:16 +0100

    Add TaskManager component and integrate with App.js

---
+++ src/components/TaskManager.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TaskManager = () => {
+  return (
+    <div>
+      <h1>Task Manager</h1>
+      <p>Start managing your tasks efficiently!</p>
+    </div>
+  );
+};
+
+export default TaskManager;
